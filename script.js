01f59a1f49f0845ba4083b3cf5e871dc2701be6b
1231945 <zivandrei.calonzo@smc.pshs.edu.ph>
2026-04-24 21:44:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+  // COOKIE FUNCTIONS
+  function setCookie(name, value, maxAge) {
+      let cookieStr = name + "=" + value + ";";
+      if (maxAge >-1) {
+         cookieStr += "max-age=" + maxAge + ";";
+      }
+      document.cookie = cookieStr;
+  }
+function getCookie(name) {
+    let nameeq = name + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i].trim();
+        if (c.indexOf(nameeq) == 0) return c.substring(nameeq.length, c.length);
+    }
+    return "";
+}
+  
+  
+  function deleteCookie(name) {
+    setCookie(name, "", -1);
+  }
+
+  // MAIN SUBMIT HANDLER
+  function handleOrder() {
+    document.getElementById("errorMsg").innerHTML = "";
+
+    // TODO: Get values
+    let name = document.getElementById("name").value;
+    let variant = document.getElementById("variant").value;
+    let quantity = document.getElementById("quantity").value;
+    let address = document.getElementById("address").value;
+    let terms = document.getElementById("terms").checked;
+    let pickup = document.getElementById("pickup").value;
+
+    // TODO: Validation
+    function validateForm(){
+    
+      if(name == "" || variant == "" || quantity == "" || (address == "" && pickup == "Delivery") || terms == "" || pickup == ""){ 
+        document.getElementById("errorMsg").innerHTML = "The following fields cannot be empty:";
+        return false;
+      }
+      if(name == ""){
+        document.getElementById("errorMsg").innerHTML += " Name";
+      }
+      if(variant == ""){
+        document.getElementById("errorMsg").innerHTML += " Variant";
+      }
+      if(quantity == ""){
+        document.getElementById("errorMsg").innerHTML += " Quantity";
+      }
+      if(address == "" && pickup == "Delivery"){
+        document.getElementById("errorMsg").innerHTML += " Address";
+      }
+      if(terms == ""){
+        document.getElementById("errorMsg").innerHTML += " Terms";
+      }
+      if(pickup == null){
+        document.getElementById("errorMsg").innerHTML += " Pickup";
+      }
+      if(quantity <= 0){
+        document.getElementById("errorMsg").innerHTML += " Quantity cannot be 0";
+      }
+      return true;
+  }
+
+    // If invalid: Display appropriate error message
+    // return false;
+    // Otherwise: Store data (cookies + localStorage)
+    if (validateForm() == false){
+      return false;
+    }
+    else{
+      setCookie("name", name, 86400);
+      localStorage.setItem("product", variant);
+        localStorage.setItem("quantity", quantity);
+        localStorage.setItem("pickup", pickup);
+        localStorage.setItem("address", address);
+
+        // Requirement: Redirect to summary.html
+        window.location.href = "summary.html";
+        return false;
+    
+  }
+  }
+    
+ 
+
+  // LOAD PREVIOUS ORDER
+  function loadPreviousOrder() {
+
+    // TODO: Retrieve stored data
+    
+        let product = localStorage.getItem("product");
+        let quantity = localStorage.getItem("quantity");
+        let pickup = localStorage.getItem("pickup");
+        let address = localStorage.getItem("address");
+
+  
+
+
+    // TODO: If no data: Display error message "No previous order data"
+    if(getCookie("name") == ""&& localStorage.getItem("product") == null && localStorage.getItem("quantity") == null && localStorage.getItem("pickup") == null && localStorage.getItem("address") == null){
+      document.getElementById("errorMsg").innerHTML = "No previous order data";
+    } 
+    // TODO: Otherwise: Populate fields
+    else{
+      document.getElementById("variant").value = localStorage.getItem("product");
+      document.getElementById("quantity").value = localStorage.getItem("quantity");
+      document.getElementById("pickup").value = localStorage.getItem("pickup");
+      document.getElementById("address").value = localStorage.getItem("address");
+      document.getElementById("terms").checked = true;
+      document.getElementById("name").value = getCookie("name");
+    }
+
+
+  }
---
+++ script2.js
@@ -0,0 +1,42 @@
+function getCookie(name) {
+    let nameeq = name + "=";
+    let c = document.cookie.split(';');
+    for (let i = 0; i < c.length; i++) {
+      let co = c[i];
+      if (co.charAt(0) == ' ') {
+        co = co.substring(1);
+      }
+      if (co.indexOf(nameeq) == 0) {
+        return co.substring(nameeq.length, co.length);
+      }
+      
+    }
+    return null;
+}
+        let product = localStorage.getItem("product");
+        let quantity = localStorage.getItem("quantity");
+        let pickup = localStorage.getItem("pickup");
+        let address = localStorage.getItem("address");
+
+  
+
+document.getElementById("Name").innerHTML = getCookie("name");
+document.getElementById("Product").innerHTML = localStorage.getItem("product");  
+document.getElementById("Quantity").innerHTML = localStorage.getItem("quantity");
+document.getElementById("Pickup").innerHTML = localStorage.getItem("pickup"); 
+
+if(product == "Antho-Presko variant1 :No Cover: 93.138 PHP"){
+  document.getElementById("price").innerHTML = localStorage.getItem("quantity") * 93.138;
+}
+else if(product == "Antho-Presko variant2 :With Cover: 143.138 PHP"){
+  document.getElementById("price").innerHTML = localStorage.getItem("quantity") * 143.138;
+}
+if(localStorage.getItem("pickup") != "Delivery"){
+    document.getElementById("Address").innerHTML= "N/A";
+}
+else if(localStorage.getItem("pickup") == "Delivery"){
+    document.getElementById("Address").innerHTML = localStorage.getItem("address");
+}
+
+
+ 
